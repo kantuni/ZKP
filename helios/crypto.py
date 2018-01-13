a3e0ba704a289f4539f17ba898733f0c82e833a4
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-01-13 20:16:12 +0400

    Update crypto.py

--- helios/crypto.py
+++ helios/crypto.py
@@ -56,6 +56,6 @@ def verify_vote(pk, cipher, proof):
     s2 = pow(g, r1, p) == a1 * pow(a, c1, p) % p
     s3 = pow(pk, r0, p) == b0 * pow(b, c0, p) % p
     s4 = pow(pk, r1, p) == b1 * pow(b * pow(g, p - 2, p) % p, c1, p) % p
-    # TODO: There is a problem with notations in the paper.
+    # TODO: There is a problem with the notation in the paper.
     # s5 = (c0 + c1) % q == custom_hash([pk, a, b, a0, b0, a1, b1])
     return s1 and s2 and s3 and s4
